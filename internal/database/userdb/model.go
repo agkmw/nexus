2f410c9b26ebdea8f5c5e6ce5a478127b0c76025
agkmw <185657819+agkmw@users.noreply.github.com>
2025-12-17 10:35:35 +0630

    feat: add user model and password type

---
+++ internal/database/userdb/model.go
@@ -0,0 +1,51 @@
+package userdb
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID         uuid.UUID
+	Username   string
+	Email      string
+	Password   password
+	CreatedAt  time.Time
+	Last_Login time.Time
+	Activated  bool
+	Version    int
+}
+
+type password struct {
+	plaintext string
+	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
